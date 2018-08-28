916a9e7c5f826e88061edfaf61dea3c673d79289
disen <610039018@qq.com>
2018-08-28 10:20:14 +0800

    创建artdb库

--- crud.py
+++ crud.py
@@ -12,12 +12,19 @@ params = {
 conn = pymysql.Connect(**params)
 print('-连接成功--')
 
-create student database ;
-
 cursor = conn.cursor()
 cursor.execute('desc user')
 for row in cursor.fetchall():
     print(row)
+
+
+print('-创建数据库artdb--')
+# cursor.execute('create database artdb')
+# cursor.execute('create table artdb.tag (id integer primary key auto_increment, name varchar(50) unique)')
+
+cursor.execute("insert artdb.tag(name) values ('军事小说'),('都市言情')")
+conn.commit()
+print(cursor.rownumber)
 
 print('--关闭数据库--')
 cursor.close()
