51157a66a9178838400d5baf19c063e0b9591bcb
disen <610039018@qq.com>
2018-08-29 16:20:58 +0800

    实现分页

--- XartPro/urls.py
+++ XartPro/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 import xadmin as admin
@@ -12,6 +13,13 @@ def index(request):
 
     # 查询所有文章Art
     arts = Art.objects.all()
+
+    # 实现Art文章的分页
+    # 获取请求参数的page页码
+    page = request.GET.get('page', '1')
+
+    paginator = Paginator(arts, 10)  # 分页器
+    pager = paginator.page(int(page))  # 读取第一页的数据
 
     return render(request, 'index.html', locals())
 
